4e926ebc5843d1900f66c13de4649476fe1e53b6
Çınar <bcinar0203@gmail.com>
2022-04-07 17:20:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// Show sidebar
+menuBtn.addEventListener("click", () => {
+    sideMenu.style.display = "block";
+});
+
+// Close sidebar
+closeBtn.addEventListener("click", () => {
+    sideMenu.style.display = "none";
+})
+
+// Change theme
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables");
+
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
+
+// Fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement("tr");
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                      `;
+    tr.innerHTML = trContent;
+    document.querySelector("table tbody").appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "15678",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "LARVENDER KF102 Drone",
+        productNumber: "56842",
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Ruko G11 Pro Drone",
+        productNumber: "56489",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: "65989",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: "15646",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "DJI Air 2S",
+        productNumber: "89897",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Lozange Drone",
+        productNumber: "000657",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    }
+]
